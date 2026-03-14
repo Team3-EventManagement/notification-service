1a00b4ea2eb2eb046e0c275982e03863c607b8c5
muhammedsinanust <muhammed.sinan@ust.com>
2026-03-14 12:54:55 +0530

    rabbit change

--- app/consumers/rabbitmq_consumer.py
+++ app/consumers/rabbitmq_consumer.py
@@ -1,21 +1,34 @@
 import pika
 import json
+import time
 from app.config import RABBITMQ_HOST
 from app.services.notification_service import process_notification
+
 
 def callback(ch, method, properties, body):
 
     event = json.loads(body)
-
     print("Received event:", event)
 
     process_notification(event)
 
+
 def start_consumer():
 
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(RABBITMQ_HOST)
-    )
+    while True:
+        try:
+            print("Connecting to RabbitMQ...")
+
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=RABBITMQ_HOST)
+            )
+
+            print("Connected to RabbitMQ")
+            break
+
+        except pika.exceptions.AMQPConnectionError:
+            print("RabbitMQ not ready, retrying in 5 seconds...")
+            time.sleep(5)
 
     channel = connection.channel()
 
